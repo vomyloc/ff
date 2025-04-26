697d44d77c51156f51b2fdbfb36264146da57b96
vomyloc <52616165+vomyloc@users.noreply.github.com>
2025-04-26 23:54:59 +0700

    Update Sh.sh

--- Sh.sh
+++ Sh.sh
@@ -1,24 +1,24 @@
-    sudo docker run -d --restart unless-stopped -it \
-    --name n8n \
-    -p 5678:5678 \
-    -e N8N_HOST="n8nfanal.anhbotdeptrai.site" \
-    -e WEBHOOK_TUNNEL_URL="https://n8nfanal.anhbotdeptrai.site/" \
-    -e WEBHOOK_URL="https://n8nfanal.anhbotdeptrai.site/" \
-    -v ~/.n8n:/root/.n8n \
-    n8nio/n8n
+# Update packages
+sudo apt update && sudo apt upgrade -y
 
+# Install Docker
+sudo apt install docker.io -y
+sudo systemctl start docker
+sudo systemctl enable docker
 
-server {
-    listen 80;
-    server_name n8nfanal.anhbotdeptrai.site; // subdomain.your-domain.com if you have a subdomain
+# Install N8N
+docker run -d \
+  --name n8n \
+  -p 5678:5678 \
+  -e N8N_HOST=n8nfanal.anhbotdeptrai.site \  # Replace with your domain
+  -v ~/.n8n:/home/node/.n8n \
+  n8nio/n8n
 
-    location / {
-        proxy_pass http://localhost:5678;
-        proxy_http_version 1.1;
-        chunked_transfer_encoding off;
-        proxy_buffering off;
-        proxy_cache off;
-        proxy_set_header Upgrade $http_upgrade;
-        proxy_set_header Connection "upgrade";
-    }
-}
+  # Install Nginx
+sudo apt install nginx -y
+
+# Install Certbot (for SSL)
+sudo apt install certbot python3-certbot-nginx -y
+
+# Get SSL certificate
+sudo certbot --nginx -d n8nfanal.anhbotdeptrai.site
